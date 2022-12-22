0ddec51e80a2dc072ad75ae636661c96ee5617be
Endre Stølsvik <endre@stolsvik.com>
2022-12-22 13:03:42 +0100

    Updated JavaDocs

--- copy_mats_javadoc.sh
+++ copy_mats_javadoc.sh
@@ -10,43 +10,79 @@
 # Only for "modern" will the API docs be copied.
 
 matsversion='0.19'
-variant='classic'
 
-if [ "$variant" = "modern" ]
-then
-  echo "MODERN! Will also copy API docs alone (mats-api)"
-elif [ "$variant" = "classic" ]
-then
-  echo "CLASSIC! Not copying API docs mats-api."
-else
-  echo "ERROR: Neither classic nor modern - stopping."
-fi
+copyJavaDoc() {
+  variant=$1
 
+  if [ "$variant" = "modern" ]; then
+    echo "MODERN! Will also copy API docs alone (mats-api)"
+  elif [ "$variant" = "classic" ]; then
+    echo "CLASSIC! Not copying API docs mats-api."
+  else
+    echo "ERROR: Neither classic nor modern - stopping."
+  fi
 
+  # Assert that we're where we should be
+  if [ -d '.git' -a -e 'EndreXY-README.txt' ]; then
+    echo "Asserted location, good to go."
+  else
+    pwd=$(pwd)
+    echo "Not in expected location, we're here: $pwd"
+    exit 1
+  fi
 
-# Assert that we're where we should be
-if [ -d '.git' -a -e 'EndreXY-README.txt' ]
-then
-  echo "Asserted location, good to go."
-else
-  pwd=`pwd`
-  echo "Not in expected location, we're here: $pwd"
-  exit 1
-fi
+  if [ "$variant" = "modern" ]; then
+    echo "Note: Variant 'modern', thus also copying API docs alone (mats-api)"
+    mkdir -p javadoc/mats3/$matsversion/api
+    echo "Deleting 'javadoc/mats3/$matsversion/api/*'"
+    rm -rf javadoc/mats3/$matsversion/api/*
+    echo "Copying in API docs from mats-api"
+    cp -r ../mats3/mats-api/build-gradle/docs/javadoc/* javadoc/mats3/$matsversion/api/
+  fi
 
-if [ "$variant" = "modern" ]
-then
-  echo "Note: Variant 'modern', thus also copying API docs alone (mats-api)"
-  mkdir -p javadoc/mats3/$matsversion/api
-  echo "Deleting 'javadoc/mats3/$matsversion/api/*'"
-  rm -rf javadoc/mats3/$matsversion/api/*
-  echo "Copying in API docs from mats-api"
-  cp -r ../mats3/mats-api/build-gradle/docs/javadoc/* javadoc/mats3/$matsversion/api/
-fi
+  # Make correct folder if not present
+  mkdir -p javadoc/mats3/$matsversion/$variant
+  echo "Deleting 'javadoc/mats3/$matsversion/$variant/*'"
+  rm -rf javadoc/mats3/$matsversion/$variant/*
+  echo "Copying in alljavadocs from root of mats3"
+  cp -r ../mats3/build-gradle/docs/javadoc/* javadoc/mats3/$matsversion/$variant/
+}
 
-# Make correct folder if not present
-mkdir -p javadoc/mats3/$matsversion/$variant
-echo "Deleting 'javadoc/mats3/$matsversion/$variant/*'"
-rm -rf javadoc/mats3/$matsversion/$variant/*
-echo "Copying in alljavadocs from root of mats3"
-cp -r ../mats3/build-gradle/docs/javadoc/* javadoc/mats3/$matsversion/$variant/
+## CLASSIC JAVADOC + JSDOC
+
+echo "## CLASSIC JAVADOC"
+echo "Setting Java to 8"
+jdk8.sh
+java -version
+
+echo "cd to mats3"
+cd ../mats3/ || exit 1
+
+echo "building javadoc"
+./gradlew clean javadoc
+
+cd ../mats3_site/ || exit 1
+
+echo "Copying JavaDoc"
+copyJavaDoc "classic"
+
+## MODERN JAVADOC + JSDOC
+
+echo "## MODERN JAVADOC + JSDOC"
+echo "Setting Java to 17"
+jdk17.sh
+java -version
+
+echo "cd to mats3"
+cd ../mats3/ || exit 1
+
+echo "building javadoc"
+./gradlew clean javadoc
+
+cd ../mats3_site/ || exit 1
+
+echo "Copying JavaDoc"
+copyJavaDoc "modern"
+
+echo "Setting Java back to 8"
+jdk8.sh
